052ae068ade04131944803aa3a9634fc7531d3a9
Yangyang Xia <Yangyang.Xia@ygomi.com>
2019-11-22 16:28:01 +0800

    restful security service

---
+++ restful-secutiry-service/src/main/java/com/s2u2m/examples/restfulsecurityservice/RestfulSecurityServiceExample.java
@@ -0,0 +1,13 @@
+package com.s2u2m.examples.restfulsecurityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulSecurityServiceExample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulSecurityServiceExample.class, args);
+    }
+
+}
